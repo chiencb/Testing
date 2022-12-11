de7fd5b3df05a44950d516673247045b2247d362
Chien Bui Cong <chien.buichong@aia.com>
2022-12-11 15:09:02 +0700

    update generate SOAP service

--- AIA-EDownload/generateSOAPService.sh
+++ AIA-EDownload/generateSOAPService.sh
@@ -4,31 +4,30 @@ TEMPLATE_ADD_PLUGIN="s/<plugins>/<plugins><plugin><groupId>org\.codehaus\.mojo<\
 CONFIG_FILE=src/main/resources/application.properties
 
 # Generate code for SOAP AIAFBusiness service
-soap_url=`grep case360.soap.address.location $CONFIG_FILE | cut -d = -f 2`
+soap_url=`grep aiafbusiness.soap.address.location $CONFIG_FILE | cut -d = -f 2`
+soap_url="$soap_url?wsdl"
 soap_url=${soap_url//\//\\/} # replace "/" to "\/"
 soap_url=${soap_url//\./\\.} # replace "." to "\."
-echo $soap_url
-# soap_package_name=`grep SOAP_PACKAGE_NAME_1 $CONFIG_FILE | cut -d = -f 2`
-# soap_package_name=${soap_package_name//\./\\.} # replace "." to "\."
-# plugin_replace_statement=${TEMPLATE_ADD_PLUGIN/WSDL_URL/$soap_url}
-# plugin_replace_statement=${plugin_replace_statement/PACKAGE_NAME/$soap_package_name}
-# echo $plugin_replace_statement
-# `sed -i $plugin_replace_statement pom.xml`
-# mvn clean jaxws:wsimport
+soap_package_name=`grep aiafbusiness.generated.package $CONFIG_FILE | cut -d = -f 2`
+soap_package_name=${soap_package_name//\./\\.} # replace "." to "\."
+plugin_replace_statement=${TEMPLATE_ADD_PLUGIN/WSDL_URL/$soap_url}
+plugin_replace_statement=${plugin_replace_statement/PACKAGE_NAME/$soap_package_name}
+`sed -i $plugin_replace_statement pom.xml`
+mvn clean jaxws:wsimport
 
-# # Generate code for SOAP case360 service
-# cp pom.xml_bk pom.xml
-# soap_url=`grep SOAP_WSDL_URL_2 $CONFIG_FILE | cut -d = -f 2`
-# soap_url=${soap_url//\//\\/} # replace "/" to "\/"
-# soap_url=${soap_url//\./\\.} # replace "." to "\."
-# soap_package_name=`grep SOAP_PACKAGE_NAME_2 $CONFIG_FILE | cut -d = -f 2`
-# soap_package_name=${soap_package_name//\./\\.} # replace "." to "\."
-# plugin_replace_statement=${TEMPLATE_ADD_PLUGIN/WSDL_URL/$soap_url}
-# plugin_replace_statement=${plugin_replace_statement/PACKAGE_NAME/$soap_package_name}
-# echo $plugin_replace_statement
-# `sed -i $plugin_replace_statement pom.xml`
-# mvn clean jaxws:wsimport
+# Generate code for SOAP case360 service
+cp pom.xml_bk pom.xml
+soap_url=`grep case360.soap.address.location $CONFIG_FILE | cut -d = -f 2`
+soap_url="$soap_url?wsdl"
+soap_url=${soap_url//\//\\/} # replace "/" to "\/"
+soap_url=${soap_url//\./\\.} # replace "." to "\."
+soap_package_name=`grep case360.generated.package $CONFIG_FILE | cut -d = -f 2`
+soap_package_name=${soap_package_name//\./\\.} # replace "." to "\."
+plugin_replace_statement=${TEMPLATE_ADD_PLUGIN/WSDL_URL/$soap_url}
+plugin_replace_statement=${plugin_replace_statement/PACKAGE_NAME/$soap_package_name}
+`sed -i $plugin_replace_statement pom.xml`
+mvn clean jaxws:wsimport
 
-# # restore pom.xml file
-# cp pom.xml_bk pom.xml
-# rm pom.xml_bk
+# restore pom.xml file
+cp pom.xml_bk pom.xml
+rm pom.xml_bk
